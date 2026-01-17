f124280c441dfac0d02c5200228cb470cb3e20db
parthazad0812 <parthazad08@gmail.com>
2026-01-17 14:28:48 +0530

    fixed trigger bug

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights, helloWorld } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
@@ -8,6 +8,5 @@ export const { GET, POST, PUT } = serve({
   functions: [
     /* your functions will be passed here later! */
     generateIndustryInsights,
-
-],
+  ],
 });
