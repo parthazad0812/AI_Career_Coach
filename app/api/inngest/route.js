cacd5f3681fbdac404fbdaf9ef19c22df6908250
parthazad0812 <parthazad08@gmail.com>
2026-01-17 14:51:08 +0530

    remove the previous update  still trying to fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,6 +2,11 @@ import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateIndustryInsights],
+  functions: [
+    /* your functions will be passed here later! */
+    generateIndustryInsights,
+  ],
+});
